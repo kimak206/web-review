255d428bf92c7228119474184e09142c78bd362b
Rizki Makarim <rizki@adplus.co.id>
2020-09-26 14:14:25 +0700

    first-commit

---
+++ js/doughnut-chart.js
@@ -0,0 +1,77 @@
+
+var ctx = document.getElementById('twitterChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: ['#3c84e8', '#a6c5ee', '#cccccc'],
+            data: [20, 30, 45],
+
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        legend: {
+            display: false,
+
+        }
+    }
+});
+
+var ctx = document.getElementById('googleChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: ['red', 'green', 'blue'],
+            data: [20, 30, 45],
+
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        legend: {
+            display: false,
+
+        }
+    }
+});
+
+var ctx = document.getElementById('facebookChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: ['yellow', 'purple', 'cyan'],
+            data: [20, 30, 45],
+
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        legend: {
+            display: false,
+
+        }
+    }
+});
+
+
